26fd85b0ae7e40c75b0ea62a177fa695151573eb
Ame Diphoko <adiphoko@Diphoko-MacBook-Pro.local>
2018-10-05 09:13:34 +0200

    configure db settings sqlite

--- tp_screening/settings.py
+++ tp_screening/settings.py
@@ -79,14 +79,14 @@ WSGI_APPLICATION = 'tp_screening.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}'''
+}
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tp_screening',
@@ -95,7 +95,7 @@ DATABASES = {
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
-}
+}'''
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
