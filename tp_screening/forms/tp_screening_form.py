50a0dc5de638d471af40a211cebfea653cb32bc6
Ame Diphoko <adiphoko@Diphoko-MacBook-Pro.local>
2018-10-15 17:12:05 +0200

    updating settings.py

--- tp_screening/forms/tp_screening_form.py
+++ tp_screening/forms/tp_screening_form.py
@@ -5,9 +5,9 @@ from ..form_validators import TpScreeningFormValidator
 
 
 class TpScreeningForm(FormValidatorMixin, forms.ModelForm):
-    
+
     form_validator_cls = TpScreeningFormValidator
-    
+
     class Meta:
         model = ParticipantScreening
         fields = '__all__'
